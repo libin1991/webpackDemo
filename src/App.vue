32df665217a77bb2feadb5cbcafbddffef54d6e5
iwangxiang <wangxianglhk@163.com>
2019-01-02 15:45:19 +0800

    2019/01/02配置

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="iVue">
+        <h1>
+            <span>hello Vue!</span>
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+    .iVue h1 span{
+        display: block;
+        background-color: red;
+        color: yellow;
+    }
+</style>
